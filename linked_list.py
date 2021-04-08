2cd98554c316100913794c2ad4d464761616e39c
Moshiur Rahman <59745926+Blurryface911@users.noreply.github.com>
2021-04-08 23:10:43 +0600

    Add files via upload

---
+++ linked_list.py
@@ -0,0 +1,77 @@
+# A python program to implement a singly linked list
+
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = Node()
+    
+
+    def append(self, data):
+        node = Node(data)
+
+        if self.head.next == None:
+            self.head.next = node
+            return
+
+        else:
+            current_node = self.head.next
+            while current_node.next:
+                current_node = current_node.next
+        
+        current_node.next = node
+
+
+    def prepend(self, data):
+        node = Node(data, self.head.next)
+        self.head.next = node
+
+
+    def insert(self, data, new_data):
+        current_node = self.head.next
+
+        while current_node:
+            if current_node.data == data:
+                new_node = Node(new_data, current_node.next)
+            
+            current_node.next = new_node
+            break
+
+        current_node = current_node.next
+
+
+    def search(self, item):
+        current_node = self.head.next
+
+        while current_node:
+            if current_node.data == item:
+                return current_node
+            
+            current_node = current_node.next
+        
+        return None
+
+
+    def remove(self, item):
+        previous_node = self.head
+        current_node = previous_node.next
+
+        while current_node:
+            if current_node.data == item:
+                break
+
+            previous_node = current_node
+            current_node = previous_node.next
+
+        if current_node == None:
+            return None
+
+        if self.head == previous_node:
+            self.head.next = current_node.next
+        else:
+            previous_node.next = current_node.next
